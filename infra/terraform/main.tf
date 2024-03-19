e708b08cf0523c424e858ce0c48da8b2034a3346
Murylo Capucho <mcapuchoribeiro@gmail.com>
2024-03-18 22:40:36 -0300

    Implementação da Infra SQS

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -42,3 +42,18 @@ resource "aws_sqs_queue_policy" "mixfast-sqs-queue-pagamento-reprovado-policy" {
   queue_url = aws_sqs_queue.mixfast-sqs-queue-pagamento-reprovado.id
   policy    = data.aws_iam_policy_document.mixfast_sqs_policy.json
 }
+
+resource "aws_sqs_queue" "mixfast-sqs-queue-cozinha" {
+  name                       = "${var.name}-notificacao-pedido-cozinha"
+  delay_seconds              = 10
+  visibility_timeout_seconds = 30
+  max_message_size           = 2048
+  message_retention_seconds  = 86400
+  receive_wait_time_seconds  = 2
+  sqs_managed_sse_enabled    = true
+}
+
+resource "aws_sqs_queue_policy" "mixfast-sqs-queue-cozinha-policy" {
+  queue_url = aws_sqs_queue.mixfast-sqs-queue-cozinha.id
+  policy    = data.aws_iam_policy_document.mixfast_sqs_policy.json
+}
